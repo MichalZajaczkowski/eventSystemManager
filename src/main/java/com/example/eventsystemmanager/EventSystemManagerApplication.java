149267d7c08f8308ea1f380fd0bd79eb5e6b3882
Michał Zajączkowski <mzajaczkowski90@gmail.com>
2023-03-13 20:22:01 +0100

    Initial commit

---
+++ src/main/java/com/example/eventsystemmanager/EventSystemManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventsystemmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSystemManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventSystemManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eventsystemmanager/EventSystemManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventsystemmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventSystemManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
