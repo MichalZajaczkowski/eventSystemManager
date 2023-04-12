8c689a3637ea9bd92d7d044c6a4bddef6268fc3d
Michał Zajączkowski <mzajaczkowski90@gmail.com>
2023-04-12 18:58:39 +0200

    add status dto

---
+++ src/main/java/com/example/eventsystemmanager/dto/StatusDto.java
@@ -0,0 +1,14 @@
+package com.example.eventsystemmanager.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class StatusDto {
+    private String name;
+    private String description;
+}
