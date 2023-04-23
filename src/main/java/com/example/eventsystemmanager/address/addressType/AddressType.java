23f0d5f4c6fd4420adce9ef42cead0e148996f6c
Michał Zajączkowski <mzajaczkowski90@gmail.com>
2023-04-23 17:11:22 +0200

    add enum AddressType

---
+++ src/main/java/com/example/eventsystemmanager/address/addressType/AddressType.java
@@ -0,0 +1,42 @@
+package com.example.eventsystemmanager.address.addressType;
+
+public enum AddressType {
+
+    USERADDRESS("User Address", 0),
+    PLACEADDRESS("Place Address", 1);
+    private final String name;
+    private final Integer value;
+
+    AddressType(String name, Integer value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public static AddressType fromValue(Integer value) {
+        for (AddressType type : AddressType.values()) {
+            if (type.getValue().equals(value)) {
+                return type;
+            }
+        }
+        throw new IllegalArgumentException("Nieprawidłowa wartość typu: " + value);
+    }
+
+    public static AddressType fromName(Integer name) {
+        for (AddressType type : AddressType.values()) {
+            if (type.getValue().equals(name)) {
+                return type;
+            }
+        }
+        throw new IllegalArgumentException("Nieprawidłowa wartość typu: " + name);
+    }
+
+
+}
