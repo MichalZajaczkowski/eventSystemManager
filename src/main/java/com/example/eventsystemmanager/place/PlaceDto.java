e6e305c9f6681908e7d6e36b2f9ea4672563b630
Michał Zajączkowski <mzajaczkowski90@gmail.com>
2023-04-23 11:53:28 +0200

    PlaceDto created

---
+++ src/main/java/com/example/eventsystemmanager/place/PlaceDto.java
@@ -0,0 +1,35 @@
+package com.example.eventsystemmanager.place;
+
+import com.example.eventsystemmanager.user.UserDto;
+import com.example.eventsystemmanager.user.userAddress.UserAddressDto;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Getter
+@Setter
+@ToString
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class PlaceDto {
+
+    private Long id;
+
+    @NotNull
+    @Valid
+    private UserAddressDto userAddressDto;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String shortName;
+
+    @NotBlank
+    private String description;
+}
