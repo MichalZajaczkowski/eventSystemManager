e95754a93d6621f572755e25766b8bd0ff521213
Michał Zajączkowski <mzajaczkowski90@gmail.com>
2023-05-01 14:39:14 +0200

    add Query findPlaceByFields

--- src/main/java/com/example/eventsystemmanager/place/PlaceRepository.java
+++ src/main/java/com/example/eventsystemmanager/place/PlaceRepository.java
@@ -1,7 +1,10 @@
 package com.example.eventsystemmanager.place;
 
+import com.example.eventsystemmanager.address.AddressEntity;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.metamodel.SingularAttribute;
@@ -25,4 +28,7 @@ public interface PlaceRepository extends JpaRepository<PlaceEntity, Long> {
     void deleteAll();
 
     Optional<Object> findById(SingularAttribute<AbstractPersistable, Serializable> id);
+
+    @Query("SELECT f FROM PlaceEntity f WHERE f.placeAddressEntity = :placeAddressEntity AND f.name = :name AND f.shortName = :shortName AND f.description = :description AND f.quantityAvailablePlaces = :quantityAvailablePlaces")
+    boolean findPlaceByFields(@Param("placeAddressEntity") AddressEntity placeAddressEntity, @Param("name") String name, @Param("shortName") String shortName, @Param("description") String description, @Param("quantityAvailablePlaces") Integer quantityAvailablePlaces);
 }
