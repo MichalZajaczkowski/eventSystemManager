55aef87076472bfa5e3da54a0ca3b2cbb2c20218
Michał Zajączkowski <mzajaczkowski90@gmail.com>
2023-05-08 19:31:16 +0200

    findUsersByAddressId, findAddressById, findUsersByUserAddress, findAddressUsers

--- src/main/java/com/example/eventsystemmanager/AdditionalEndpoints/AdditionalRepository.java
+++ src/main/java/com/example/eventsystemmanager/AdditionalEndpoints/AdditionalRepository.java
@@ -1,14 +1,17 @@
 package com.example.eventsystemmanager.AdditionalEndpoints;
 
+import com.example.eventsystemmanager.address.AddressEntity;
 import com.example.eventsystemmanager.event.EventEntity;
 import com.example.eventsystemmanager.organizer.OrganizerEntity;
 import com.example.eventsystemmanager.place.PlaceEntity;
+import com.example.eventsystemmanager.user.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface AdditionalRepository extends JpaRepository<EventEntity, Long> {
 
     @Query("SELECT e FROM EventEntity e JOIN e.place p JOIN e.organizer o WHERE p.name LIKE %:placeName% OR o.name LIKE %:organizerName%")
@@ -26,6 +29,19 @@ public interface AdditionalRepository extends JpaRepository<EventEntity, Long> {
 
     @Query("SELECT e FROM EventEntity e JOIN e.place p WHERE p.name = :placeName")
     List<EventEntity> findByPlaceName(@Param("placeName") String placeName);
+
+    @Query("SELECT u FROM UserEntity u WHERE u.addressEntity.id = :addressId")
+    List<UserEntity> findUsersByAddressId(Long addressId);
+
+    @Query("SELECT a FROM AddressEntity a WHERE a.id = :addressId")
+    AddressEntity findAddressById(Long addressId);
+
+    @Query("SELECT u FROM UserEntity u WHERE u.addressEntity.addressType = 'USER_ADDRESS'")
+    List<UserEntity> findUsersByUserAddress();
+
+    @Query("SELECT u, a FROM UserEntity u JOIN u.addressEntity a WHERE a.id IN (SELECT ua.addressEntity.id FROM UserEntity ua GROUP BY ua.addressEntity.id HAVING COUNT(DISTINCT ua.id) > 1)")
+    List<Object[]> findAddressUsers();
+
 
 
 }
