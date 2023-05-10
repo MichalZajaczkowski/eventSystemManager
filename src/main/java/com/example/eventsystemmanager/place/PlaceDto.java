e3d3e5d6585d4306e0ec04542c6c8e6dbf035ad8
Michał Zajączkowski <mzajaczkowski90@gmail.com>
2023-05-10 19:22:03 +0200

    add Valid and createdDate modifiedDate

--- src/main/java/com/example/eventsystemmanager/place/PlaceDto.java
+++ src/main/java/com/example/eventsystemmanager/place/PlaceDto.java
@@ -4,11 +4,13 @@ package com.example.eventsystemmanager.place;
 
 import com.example.eventsystemmanager.address.AddressDto;
 import com.example.eventsystemmanager.address.AddressEntity;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 
 import javax.validation.Valid;
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.*;
+import java.time.LocalDateTime;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -20,19 +22,32 @@ import javax.validation.constraints.NotBlank;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class PlaceDto {
 
-    @NotBlank
+    @Positive(message = "Place ID must be a positive number")
     private Long id;
+
+
     @Valid
     private AddressDto placeAddress;
-    @NotBlank
-    private String name;
-    @NotBlank
-    private String shortName;
-    @NotBlank
-    private String description;
-    private Integer quantityAvailablePlaces;
-//    private PlaceStatus placeStatus;
 
+    @Size(min = 2, max = 50, message = "Place name must be between 2 and 50 characters long")
+    private String name;
+
+    @Size(min = 2, max = 20, message = "Place short name must be between 2 and 20 characters long")
+    private String shortName;
+
+    @Size(min = 10, max = 500, message = "Place description must be between 10 and 500 characters long")
+    private String description;
+
+    @NotNull(message = "Available places quantity must be specified")
+    @Min(value = 0, message = "Available places quantity must be a non-negative number")
+    @Max(value = 10000, message = "Available places quantity must be less than or equal to 10000")
+    private Integer quantityAvailablePlaces;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdDate;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime modifiedDate;
 
     public PlaceDto(PlaceEntity placeEntity) {
         this.id = placeEntity.getId();
@@ -45,19 +60,6 @@ public class PlaceDto {
     public void setPlaceAddressToDto(AddressEntity addressEntity) {
         this.placeAddress = new AddressDto(addressEntity);
     }
-
-
-//    public PlaceEntity toPlaceEntity() {
-//        return new PlaceEntity(
-//                id,
-//                placeAddress.toAddressEntity(),
-//                name,
-//                shortName,
-//                description,
-//                quantityAvailablePlaces
-//        );
-//    }
-
 
     public PlaceEntity toPlaceEntity() {
         PlaceEntity placeEntity = new PlaceEntity();
@@ -72,3 +74,4 @@ public class PlaceDto {
         return placeEntity;
     }
 }
+
