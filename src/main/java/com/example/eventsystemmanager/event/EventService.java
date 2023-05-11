c91628e59dc0aae4bab98a871aa4c394ded30f21
Michał Zajączkowski <mzajaczkowski90@gmail.com>
2023-05-11 22:01:10 +0200

    delete findAll, changes in updatePlaceForEvent method and updateAllEventStatus, updateSingleEventStatus, add setStatus, EnableScheduling

--- src/main/java/com/example/eventsystemmanager/event/EventService.java
+++ src/main/java/com/example/eventsystemmanager/event/EventService.java
@@ -15,16 +15,13 @@ import com.example.eventsystemmanager.place.PlaceRepository;
 import com.example.eventsystemmanager.place.PlaceService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import java.util.Optional;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
-
 import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 
 @RequiredArgsConstructor
@@ -34,19 +31,13 @@ public class EventService {
     private static final String PLACE_WITH_ID_DOES_NOT_EXIST = "Place with id " + id + " does not exist.";
     private static final String ORGANIZER_WITH_ID_DOES_NOT_EXIST = "Organizer with id " + id + " does not exist.";
     private static final String EVENT_WITH_ID_DOES_NOT_EXIST = "Event with id " + id + " does not exist.";
+    private static final String PLACE_WITH_NAME_DOES_NOT_EXIST = "Place does not exist.";
     private final EventRepository eventRepository;
     private final OrganizerRepository organizerRepository;
     private final PlaceRepository placeRepository;
     private final OrganizerService organizerService;
     private final PlaceService placeService;
     private final EventMapper eventMapper;
-
-    public List<EventDto> findAll() {
-        return eventRepository.findAll()
-                .stream()
-                .map(this::mapEventToDto)
-                .toList();
-    }
 
     public List<EventDto> findAllEvents() {
         return eventRepository.findAll()
@@ -166,33 +157,53 @@ public class EventService {
         }
         if (eventDto.getEventStartDate() != null) {
             eventToUpdate.setEventStartDate(eventDto.getEventStartDate());
+            eventToUpdate.setEventStatus(EventStatus.POSTPONED_UPCOMING);
         }
         if (eventDto.getEventEndDate() != null) {
             eventToUpdate.setEventEndDate(eventDto.getEventEndDate());
+            eventToUpdate.setEventStatus(EventStatus.POSTPONED_UPCOMING);
         }
         eventToUpdate.setModifiedDate(LocalDateTime.now());
         eventRepository.save(eventToUpdate);
         return eventDto;
     }
 
-    public EventDto updatePlaceForEvent(Long eventId, PlaceDto placeId) {
+    public EventDto updatePlaceForEvent(Long eventId, PlaceDto place) {
         EventEntity eventToUpdate = eventRepository.findById(eventId)
                 .orElseThrow(() -> new IllegalArgumentException(EVENT_WITH_ID_DOES_NOT_EXIST));
 
         // Jeśli miejsce nie zostanie zmienione, to nie wykonujemy żadnych akcji
-        if (placeId.equals(eventToUpdate.getPlace().getId())) {
+        if (place.equals(eventToUpdate.getPlace())) {
             return eventMapper.toDto(eventToUpdate);
         }
-        // Pobierz miejsce, które chcemy przypisać do wydarzenia
-        PlaceEntity placeToUpdate = placeRepository.findById(placeId.getId())
-                .orElseThrow(() -> new IllegalArgumentException(PLACE_WITH_ID_DOES_NOT_EXIST));
+
+        PlaceEntity placeToUpdate;
+        if (place.getId() != null) {
+            placeToUpdate = placeRepository.findById(place.getId())
+                    .orElseThrow(() -> new IllegalArgumentException(PLACE_WITH_ID_DOES_NOT_EXIST));
+        } else if (place.getName() != null) {
+            Optional<PlaceEntity> optionalPlace = Optional.ofNullable(placeRepository.findByName(place.getName()));
+            if (optionalPlace.isEmpty()) {
+                throw new IllegalArgumentException(PLACE_WITH_NAME_DOES_NOT_EXIST);
+            }
+            placeToUpdate = optionalPlace.get();
+        } else {
+            throw new IllegalArgumentException("Invalid place data. Either ID or name must be provided.");
+        }
+
         // Ustaw zmodyfikowaną wartość miejsca w wydarzeniu
         eventToUpdate.setPlace(placeToUpdate);
-        eventToUpdate.setModifiedDate(LocalDateTime.now()); // ustawiamy pole modifyDate
+        eventToUpdate.setModifiedDate(LocalDateTime.now());
+        // Zmiana statusu na POSTPONED_UPCOMING
+        if (eventToUpdate.getEventStatus() != EventStatus.FINISHED) {
+            // Zmiana statusu na POSTPONED_UPCOMING
+            eventToUpdate.setEventStatus(EventStatus.POSTPONED_UPCOMING);
+        }
 
         EventEntity updatedEvent = eventRepository.save(eventToUpdate);
         return eventMapper.toDto(updatedEvent);
     }
+
     public EventDto updateOrganizerForEvent(Long eventId, OrganizerDto organizerId) {
         EventEntity eventToUpdate = eventRepository.findById(eventId)
                 .orElseThrow(() -> new IllegalStateException(EVENT_WITH_ID_DOES_NOT_EXIST));
@@ -212,7 +223,17 @@ public class EventService {
         return eventMapper.toDto(updatedEvent);
     }
 
-    public void updateEventStatus(Long eventId) {
+    public void updateAllEventStatus() {
+        List<EventEntity> events = eventRepository.findAll();
+
+        for (EventEntity eventEntity : events) {
+            updateSingleEventStatus(eventEntity.getId());
+        }
+
+        eventRepository.saveAll(events);
+    }
+
+    public void updateSingleEventStatus(Long eventId) {
         EventEntity eventEntity = eventRepository.findById(eventId)
                 .orElseThrow(() -> new IllegalStateException(EVENT_WITH_ID_DOES_NOT_EXIST));
 
@@ -220,23 +241,69 @@ public class EventService {
         LocalDateTime eventEndDate = eventEntity.getEventEndDate();
         LocalDateTime now = LocalDateTime.now();
 
-        if(eventEntity.getEventStatus() == EventStatus.CANCELLED) {
+        EventStatus currentStatus = eventEntity.getEventStatus();
+
+        if (currentStatus == EventStatus.CANCELLED) {
             return; // event has been cancelled, no need to update
-        } else if(eventEntity.getEventStatus() == EventStatus.POSTPONED_UPCOMING) {
-            if(now.isBefore(eventStartDate)) {
-                eventEntity.setEventStatus(EventStatus.UPCOMING);
+        } else if (currentStatus == EventStatus.POSTPONED_UPCOMING) {
+            if (now.isAfter(eventEndDate)) {
+                eventEntity.setEventStatus(EventStatus.FINISHED);
             } else {
                 return; // event is postponed, no need to update unless it's upcoming
             }
-        } else if (now.isBefore(eventStartDate)) {
-            eventEntity.setEventStatus(EventStatus.UPCOMING);
-        } else if (now.isAfter(eventEndDate)) {
-            eventEntity.setEventStatus(EventStatus.FINISHED);
+        } else if (currentStatus == EventStatus.FINISHED) {
+            return; // event is finished, no need to update
         } else {
-            eventEntity.setEventStatus(EventStatus.IN_PROGRESS);
+            if (now.isBefore(eventStartDate)) {
+                eventEntity.setEventStatus(EventStatus.UPCOMING);
+            } else if (now.isAfter(eventEndDate)) {
+                eventEntity.setEventStatus(EventStatus.FINISHED);
+            } else {
+                eventEntity.setEventStatus(EventStatus.IN_PROGRESS);
+            }
         }
         eventRepository.save(eventEntity);
     }
+
+    public void setStatus(Long eventId, String status) {
+        EventEntity eventEntity = eventRepository.findById(eventId)
+                .orElseThrow(() -> new IllegalArgumentException(EVENT_WITH_ID_DOES_NOT_EXIST));
+
+        EventStatus eventStatus = null;
+        try {
+            // Sprawdzamy, czy status może być zinterpretowany jako Enum EventStatus
+            eventStatus = EventStatus.valueOf(status.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            // Jeśli podany status nie jest poprawnym Enumem, sprawdzamy czy istnieje EventStatus o podanej nazwie
+            for (EventStatus enumStatus : EventStatus.values()) {
+                if (enumStatus.getStatus().equalsIgnoreCase(status)) {
+                    eventStatus = enumStatus;
+                    break;
+                }
+            }
+        }
+
+        if (eventStatus == null) {
+            throw new IllegalArgumentException("Invalid event status: " + status);
+        }
+
+        eventEntity.setEventStatus(eventStatus);
+
+        if (eventStatus == EventStatus.CANCELLED) {
+            eventEntity.setEventStartDate(null);
+            eventEntity.setEventEndDate(null);
+            eventEntity.setPlace(null);
+        }
+
+        eventRepository.save(eventEntity);
+    }
+
+
+//    @Scheduled(fixedRate = 3600000) // Aktualizacja co 1 godzinę (3600000 ms)
+//    public void scheduleEventStatusUpdate() {
+//        updateEventStatus();
+//    }
+
     private EventDto mapEventToDto(EventEntity eventEntity) {
         EventDto eventDto = new EventDto();
         eventDto.setId(eventEntity.getId());
